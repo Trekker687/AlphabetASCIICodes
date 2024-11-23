d34ab7630502ef14d1082767a3bcc1ba2c500f0c
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-11-23 11:46:24 +0530

    Add files via upload

---
+++ AlphabetASCIICodes/main.py
@@ -0,0 +1,108 @@
+alphabet = input("Enter a alphabet")
+
+if alphabet == 'A':
+    print("ASCII code is 65")
+elif alphabet == "B":
+    print("ASCII code is 66")
+elif alphabet == "C":
+    print("ASCII code is 67")
+elif alphabet == "D":
+    print("ASCII code is 68")
+elif alphabet == "E":
+    print("ASCII code is 69")
+elif alphabet == "F":
+    print("ASCII code is 70")
+elif alphabet == "G":
+    print("ASCII code is 71")
+elif alphabet == "H":
+    print("ASCII code is 72")
+elif alphabet == "I":
+    print("ASCII code is 73")
+elif alphabet == "J":
+    print("ASCII code is 74")
+elif alphabet == "K":
+    print("ASCII code is 75")
+elif alphabet == "L":
+    print("ASCII code is 76")
+elif alphabet == "M":
+    print("ASCII code is 77")
+elif alphabet == "N":
+    print("ASCII code is 78")
+elif alphabet == "O":
+    print("ASCII code is 79")
+elif alphabet == "P":
+    print("ASCII code is 80")
+elif alphabet == "Q":
+    print("ASCII code is 81")
+elif alphabet == "R":
+    print("ASCII code is 82")
+elif alphabet == "S":
+    print("ASCII code is 83")
+elif alphabet == "T":
+    print("ASCII code is 84")    
+elif alphabet == "U":
+    print("ASCII code is 85")
+elif alphabet == "V":
+    print("ASCII code is 86")
+elif alphabet == "W":
+    print("ASCII code is 87")
+elif alphabet == "X":
+    print("ASCII code is 88")
+elif alphabet == "Y":
+    print("ASCII code is 89")
+elif alphabet == "Z":
+    print("ASCII code is 90")
+elif alphabet == "a":
+    print("ASCII code is 98")
+elif alphabet == "b":
+    print("ASCII code is 99")
+elif alphabet == "c":
+    print("ASCII code is 100")
+elif alphabet == "d":
+    print("ASCII code is 101")
+elif alphabet == "e":
+    print("ASCII code is 102")
+elif alphabet == "f":
+    print("ASCII code is 103")
+elif alphabet == "g":
+    print("ASCII code is 104")
+elif alphabet == "h":
+    print("ASCII code is 105")
+elif alphabet == "i":
+    print("ASCII code is 106")
+elif alphabet == "j":
+    print("ASCII code is 107")
+elif alphabet == "k":
+    print("ASCII code is 108")
+elif alphabet == "l":
+    print("ASCII code is 109")
+elif alphabet == "m":
+    print("ASCII code is 110")
+elif alphabet == "n":
+    print("ASCII code is 111")
+elif alphabet == "o":
+    print("ASCII code is 112")
+elif alphabet == "p":
+    print("ASCII code is 113")
+elif alphabet == "q":
+    print("ASCII code is 114")
+elif alphabet == "r":
+    print("ASCII code is 115")
+elif alphabet == "s":
+    print("ASCII code is 116")
+elif alphabet == "t":
+    print("ASCII code is 117")    
+elif alphabet == "u":
+    print("ASCII code is 118")
+elif alphabet == "v":
+    print("ASCII code is 119")
+elif alphabet == "w":
+    print("ASCII code is 120")
+elif alphabet == "x":
+    print("ASCII code is 121")
+elif alphabet == "y":
+    print("ASCII code is 122")
+elif alphabet == "z":
+    print("ASCII code is 123")
+else:
+    print("Invalid character")
